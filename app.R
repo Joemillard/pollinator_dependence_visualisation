ddd10b811e854af14e018afb5e39a2100b774161
joemillard <joeymillard@hotmail.co.uk>
2021-05-12 10:23:37 +0100

    amend app.r file an change file names to remove extra .R

--- app.R
+++ app.R
@@ -1,33 +1,24 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# shiny app for visualising change in pollination dependent vulnerability
 
 library(shiny)
+library(dplyr)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Change in global pollination dependence vulnerability"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            sliderInput("time",
+                        "Number of bins:")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("maplot")
         )
     )
 )
@@ -35,13 +26,30 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # read in the map data
+    map_data <- readRDS("data/global_change_pollination_dependence.rds")
+    
+    output$maplot<- renderPlot({
+        
+        # plot the ggplot map for climate anomaly
+        map_data[[1]] %>%
+            ggplot() +
+            geom_polygon(aes(x = long, y = lat, group = group), data = map_fort, fill = "grey", alpha = 0.3) +
+            geom_tile(aes(x = x, y = y, fill = poll_vulnerability)) +
+            ggtitle("2050") +
+            scale_fill_viridis("Vulnerability-weighted \npollination dependence)",
+                               na.value = "transparent", option = "plasma", direction = -1,
+                               limits = c(0, 1), breaks = c(0, 0.25, 0.5, 0.75, 1), labels = c("0", "0.25", "0.5", "0.75", "1")) +
+            coord_equal() +
+            guides(fill = guide_colourbar(ticks = FALSE)) +
+            theme(panel.background = element_blank(),
+                  panel.bord = element_blank(),
+                  panel.grid = element_blank(), 
+                  axis.text = element_blank(),
+                  axis.ticks = element_blank(), 
+                  axis.title = element_blank(),
+                  legend.position = "none")
+            
     })
 }
 
